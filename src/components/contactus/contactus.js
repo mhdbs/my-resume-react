f44ab18102de597da0f040529d9892c5f5cb88f7
bilal <mohammedbilal1995@gmail.com>
2019-04-24 20:19:46 +0530

    first commit

---
+++ src/components/contactus/contactus.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            {/* <div className="ten columns"> */}
+              {/* <p className="lead">
+              
+              </p> */}
+            {/* </div> */}
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>Linked in :
+                  {resumeData.linkedinId}
+                  <h4>
+                  Email :  
+                    {resumeData.gmail}
+                  </h4>
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Mohammed Bilal",
+  "role": "Fullstack Developer",
+  "linkedinId":"https://www.linkedin.com/in/mohammed-bilal-a62477154/",
+  "gmail": "mohammedbilal1995@gmail.com",
+  "skypeid": "mohammedbilal1995@gmail.com",
+  "roleDescription": "",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/mohammed-bilal-a62477154/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/mhdbs",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"mohammedbilal1995@gmail.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"Self-taught Programmer, Love to develop machines, I have done human recognition and integrated with a rover for a real time project and it is displayed in MAKKAL TV on behalf of DLK Technologies Pvt Ltd. Developed Authentication service in Golang and Mongodb as a database, with csrf, auth-token and refresh-token verification.I have done log-tail for tailing the logs in one single websocket page where logs are generated from microservices in Golang. Developed realtime IOT appliances API Gateway in Golang with vernemq and mqtt broker as a protocol, those projects are all developed for my own personal purposes.",
+  "address":"Chennai, India",
+  "website":"https://www.mhdbs.com",
+  "mail": "mohammedbilal1995@gmail.com",
+  "education":[
+    {
+      "UniversityName":"Mohamed Sathak AJ College of Engineering",
+      "specialization":"Electrical and Electronics Engineering",
+      "MonthOfPassing":"Jun",
+      "YearOfPassing":"2017"
+      // "Achievements":""
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"XYZ Innovations Pvt Ltd",
+      "specialization":"Software Engineer",
+      "MonthOfLeaving":"Dec 2018 - Current",
+      // "YearOfLeaving":"2018",
+      "Achievements":""
+    },
+    {
+      "CompanyName":"Smartron India Pvt Ltd",
+      "specialization":"Software Engineer",
+      "MonthOfLeaving":"April 2018 - Dec",
+      "YearOfLeaving":"2018",
+      "Achievements":""
+    },
+    {
+      "CompanyName":"DLK Technologies Pvt Ltd",
+      "specialization":"Embedded Developer",
+      "MonthOfLeaving":"Sept 2017 - April",
+      "YearOfLeaving":"2018",
+      "Achievements":""
+    }
+  ],
+  "skillsDescription":"",
+  "skills":[
+    {
+      "skillname":"Node JS"
+    },
+    {
+      "skillname":"Golang"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"",
+      "name":""
+    },
+    {
+      "description":"",
+      "name":""
+    }
+  ]
+}
+
+export default resumeData
